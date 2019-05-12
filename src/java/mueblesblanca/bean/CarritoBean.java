a7d8284c412f87b39342c256bd5f94cd441cf7dd
SerggioAn27 <serggios_17@hotmaill.com>
2019-05-12 01:33:47 -0500

    La base del carrito de compras, que errores tengo, no puedo eliminar los productos del carrito, el total no se muestra, y en la parte del banner al vaciar el carrirto no se vacia se quita el valor del banner, es decir, queda en 0 pero los productos no se quitan y falta  que cuando el cliente le de ver al detalles del producto, hay tambien hay un btón adicionar carrito que desde hay tambien se pueda adicionar el producto al carrito

--- src/java/mueblesblanca/bean/CarritoBean.java
+++ src/java/mueblesblanca/bean/CarritoBean.java
@@ -10,9 +10,12 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.faces.application.Application;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.ViewScoped;
+import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpSession;
+import mueblesblanca.util.HibernateUtil;
 import mueblesblanca.vo.ArticuloVO;
 import mueblesblanca.vo.ProductoVO;
 
@@ -22,12 +25,11 @@ import mueblesblanca.vo.ProductoVO;
  */
 
 @ManagedBean(name = "carritoBean")
-@ViewScoped
+@SessionScoped
 public class CarritoBean implements Serializable {
 
     private static long serialVersionUID = 4545919119678482516L;
     
-    //Variables
     private List<ArticuloVO> articulo = new ArrayList<ArticuloVO>();
     private ArticuloVO selectedArticulo = new ArticuloVO();
     private float totalCarrito;
@@ -38,25 +40,6 @@ public class CarritoBean implements Serializable {
             FacesContext context = FacesContext.getCurrentInstance();
             Application application = context.getApplication();
         }
-    }
-    
-    public List<ArticuloVO> AñadirAlCarrito (ProductoVO producto, Integer cantidad){
-        for (ArticuloVO articulovo : articulo) {
-            if (articulovo.getProducto().getIdProducto()==producto.getIdProducto()){
-                articulovo.setCantidad(cantidad + articulovo.getCantidad());
-                articulovo.setId(producto.getIdProducto());
-                return articulo;
-            }
-        }
-        ArticuloVO articulovo1 = new ArticuloVO();
-        articulovo1.setCantidad(cantidad);
-        articulovo1.setProducto(producto);
-        articulovo1.setValorTotal(1);
-        articulovo1.setId(producto.getIdProducto());
-        articulo.add(articulovo1);
-        
-        return articulo;
-
     }
     
     public static long getSerialVersionUID() {
@@ -75,7 +58,11 @@ public class CarritoBean implements Serializable {
         this.articulo = articulo;
     }
 
-    public float getTotalCarrito() {
+    public float getTotalCarrito() {        
+        totalCarrito = 0;
+        for (ArticuloVO item : articulo) {
+            totalCarrito += item.getValorTotal();
+        }
         return totalCarrito;
     }
 
@@ -90,5 +77,51 @@ public class CarritoBean implements Serializable {
     public void setSelectedArticulo(ArticuloVO selectedArticulo) {
         this.selectedArticulo = selectedArticulo;
     }
-  
+    
+    public List<ArticuloVO> AñadirAlCarrito (ProductoVO producto, Integer cantidad ){
+        HttpSession session = HibernateUtil.getSession();
+
+        if ((List<ArticuloVO>) session.getAttribute("carrito") != null) {
+            articulo = (List<ArticuloVO>) session.getAttribute("carrito");
+        }
+        System.out.println("mueblesblanca.bean.CarritoBean.AñadirAlCarrito()");
+        for (ArticuloVO articulovo : articulo) {
+            if (articulovo.getProducto().getIdProducto().equals(producto.getIdProducto())){
+                articulovo.setCantidad(cantidad + articulovo.getCantidad());
+                articulovo.setId(producto.getIdProducto());
+                return articulo;
+            }
+        }
+        ArticuloVO articulovo1 = new ArticuloVO();
+        articulovo1.setCantidad(cantidad);
+        articulovo1.setProducto(producto);
+        articulovo1.setValorTotal(1);
+        articulovo1.setId(producto.getIdProducto());
+        articulo.add(articulovo1);
+        session.setAttribute("carrito", this.articulo);
+        System.out.println(articulo);
+        
+        return articulo;
+
+    }
+    
+    public void remove(){
+        String msg;
+        HttpSession session = HibernateUtil.getSession();
+        List<ArticuloVO> lista = (List<ArticuloVO>) session.getAttribute("carrito");
+            lista = (List<ArticuloVO>) session.getAttribute("carrito");
+
+        for (ArticuloVO item : lista) {
+            if(item.getId().equals(selectedArticulo.getId()))
+            {
+                lista.remove(selectedArticulo);
+                
+                msg = "Se elimino correctamente el producto";
+                FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, msg, null);
+                FacesContext.getCurrentInstance().addMessage(null, message);
+            }
+        }
+         session.setAttribute("carrito", lista);
+    }
+ 
 }
---
+++ src/java/mueblesblanca/util/HibernateUtil.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mueblesblanca.util;
+
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
+
+public class HibernateUtil {
+    private static SessionFactory sessionFactory;
+    private static ServiceRegistry serviceRegistry;
+
+    public static SessionFactory createSessionFactory() {
+        Configuration configuration = new Configuration();
+        configuration.configure("hibernate.cfg.xml");
+        StandardServiceRegistryBuilder ssrb = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties());
+        sessionFactory = configuration.buildSessionFactory(ssrb.build());
+        
+      return sessionFactory;
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return createSessionFactory();
+    }
+    
+    public static HttpSession getSession(){
+        return (HttpSession) FacesContext.getCurrentInstance().getExternalContext().getSession(false);
+    }
+    
+    public static HttpServletRequest getRequest(){
+        return (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest();
+    }
+}
