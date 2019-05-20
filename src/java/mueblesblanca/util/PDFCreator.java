7b44ec4779cf362582c1810cd1aef9e567ebead3
Alexis Holguin <wholguinmor@uniminuto.edu.co>
2019-05-20 03:12:20 -0500

    arreglo de otro bug

--- src/java/mueblesblanca/util/PDFCreator.java
+++ src/java/mueblesblanca/util/PDFCreator.java
@@ -11,23 +11,22 @@ import com.itextpdf.kernel.pdf.PdfWriter;
 import com.itextpdf.layout.Document; 
 import com.itextpdf.layout.element.Paragraph;  
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
 /**
  *
  * @author Alexis Holguin github:MoraHol
  */
 public class PDFCreator {
-    public void generateBill(String route, OrdenCompraVO ordenCompra) throws FileNotFoundException{
-        PdfWriter writer = new PdfWriter(route);
-        PdfDocument pdf = new PdfDocument(writer);   
-        Document document = new Document(pdf);
-        String para1 = "Tutorials Point originated from the idea that there exists"
-                + " a class of readers who respond better to online content and prefer to learn "
-                + "new skills at their own pace from the comforts of their drawing rooms.";
-        Paragraph p1 = new Paragraph(para1);
-        
-        document.add(p1);
-        document.close();
+    public void generateBill(String route, OrdenCompraVO ordenCompra) throws FileNotFoundException, IOException{
+        try (PdfWriter writer = new PdfWriter(route); PdfDocument pdf = new PdfDocument(writer); Document document = new Document(pdf)) {
+            String para1 = "Tutorials Point originated from the idea that there exists"
+                    + " a class of readers who respond better to online content and prefer to learn "
+                    + "new skills at their own pace from the comforts of their drawing rooms.";
+            Paragraph p1 = new Paragraph(para1);
+            
+            document.add(p1);
+        }
         System.out.println("document saved");
     }
 }
